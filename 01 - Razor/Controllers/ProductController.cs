072136860056988ca5cc45e2829bbf5a67e65392
Maja Risek <108302992+mrisek13@users.noreply.github.com>
2024-03-11 21:48:31 +0100

    Add files via upload

---
+++ 01 - Razor/Controllers/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VjezbaRazor.Models;
+
+namespace VjezbaRazor.Controllers
+{
+    public class ProductController : Controller
+    {
+        Product mojProduct = new Product {
+            ProductID = 1,
+            Naziv = "Reket",
+            Opis = "Nogometna lopta",
+            Cijena = 852,
+            Kategorija = "Sport"
+        };
+
+        // GET: Product
+        public ActionResult ProductWeb()
+        {
+            // odredimo trenutno vrijeme na serveru
+            int sati = DateTime.Now.Hour;
+            ViewBag.Pozdrav = sati < 12 ? "Želim dobro jutro!" : "Ugodno popodne.";
+            return View(mojProduct);
+        }
+    }
+}
---
+++ 01 - Razor/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VjezbaRazor.Models
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string Naziv { get; set; }
+        public string Opis { get; set; }
+        public decimal Cijena { get; set; }
+        public string Kategorija { set; get; }
+    }
+}
