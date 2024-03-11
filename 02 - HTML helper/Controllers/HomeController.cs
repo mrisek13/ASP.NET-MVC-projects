dd28f849ce306d990e3d9057cca6b8e4227b1643
Maja Risek <108302992+mrisek13@users.noreply.github.com>
2024-03-11 21:45:10 +0100

    Add files via upload

---
+++ 02 - HTML helper/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using PAUP_Vj2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PAUP_Vj2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Primjer1()
+        {
+            ViewBag.Message = "Primjer korištenja HTML.ActionLink elementa.";
+
+            return View();
+        }
+
+        public ActionResult Primjer2()
+        {
+            ViewBag.Message = "Primjer 2.";
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Primjer2(Student _student)
+        {
+            ViewBag.Message = "Primjer 2 - TextBox.";
+
+            return View("Student", _student);
+        }
+
+        public ActionResult Student() {
+            ViewBag.Message = "Upisani podaci o studentu";
+
+            return View();
+        }
+
+        public ActionResult Primjer3()
+        {
+            ViewBag.Message = "Primjer 3.";
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Primjer3(Student _student)
+        {
+            ViewBag.Message = "Primjer 3 - TextBox.";
+
+            return View("Student2", _student);
+        }
+    }
+}
---
+++ 02 - HTML helper/Models/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PAUP_Vj2.Models
+{
+    public class Student
+    {
+        [Display(Name ="Ime studenta")]
+        public string Ime { get; set; }
+
+        [Display(Name = "Prezime studenta")]
+        public string Prezime { get; set; }
+
+        [Display(Name = "Godina rođenja")]
+        public int godina { get; set; }
+
+        [Display(Name = "Spol")]
+        public string spol { get; set; }
+
+        [Display(Name = "Godina studija")]
+        public int godina_studija { get; set; }
+    }
+}
